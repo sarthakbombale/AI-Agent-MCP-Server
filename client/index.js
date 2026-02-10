07ebbad5a354bef79db702a07f2e89dd69b902e8
sarthakbombale <sarthakbomble250@gmail.com>
2026-02-10 23:25:42 +0530

    imports fix

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import { config } from "dotenv";
-
+import { Readline } from "readline/promises";
 import { GoogleGenAI } from "@google/genai";
+import {Client} from "@modelcontextprotocol/sdk/client/index.js";
+import {SSEClientTransport} from "@modelcontextprotocol/sdk/client/sse.js"
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
